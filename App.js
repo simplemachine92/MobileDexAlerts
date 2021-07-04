729897ae639fe85c3bd5ca04dcea6e6c13836658
simplemachine92 <justinpulley92@gmail.com>
2021-07-04 18:46:03 -0500

    Cornerstone

--- App.js
+++ App.js
@@ -1,14 +1,73 @@
 import React from 'react';
+import { AppRegistry } from 'react-native';
+import { gql, useQuery, ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { HttpLink } from 'apollo-link-http';
 import { Text, View } from 'react-native';
 
-const YourApp = () => {
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+  cache: new InMemoryCache()
+});
+
+const DAI_QUERY = gql`
+  query tokens($tokenAddress: Bytes!) {
+    tokens(where: { id: $tokenAddress }) {
+      derivedETH
+      totalLiquidity
+    }
+  }
+`
+
+const ETH_PRICE_QUERY = gql`
+  query bundles {
+    bundles(where: { id: "1" }) {
+      ethPrice
+    }
+  }
+`
+
+function Prices() {
+  const { loading: ethLoading, data: ethPriceData } = useQuery(ETH_PRICE_QUERY)
+  const { loading: daiLoading, data: daiData } = useQuery(DAI_QUERY, {
+    variables: {
+      tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f'
+    }
+  })
+
+  const daiPriceInEth = daiData && daiData.tokens[0].derivedETH
+  const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity
+  const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
-    </View>
-  );
+    <div>
+      <div>
+        Dai price:{' '}
+        {ethLoading || daiLoading
+          ? 'Loading token data...'
+          : '$' +
+            // parse responses as floats and fix to 2 decimals
+            (parseFloat(daiPriceInEth) * parseFloat(ethPriceInUSD)).toFixed(2)}
+      </div>
+      <div>
+        Dai total liquidity:{' '}
+        {daiLoading
+          ? 'Loading token data...'
+          : // display the total amount of DAI spread across all pools
+            parseFloat(daiTotalLiquidity).toFixed(0)}
+      </div>
+    </div>
+  )
 }
 
-export default YourApp;
+const App = () => (
+  <ApolloProvider client={client}>
+   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+     <Prices /> 
+    </View>
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent('MyApplication', () => App);
+
+
+export default App;
